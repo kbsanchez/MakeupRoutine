5e0ce6c1672ce4d9ee471d8f4b542f415fe0dc36
kbsanchez <66031870+kbsanchez@users.noreply.github.com>
2022-05-18 14:03:53 -0400

    Initial Commit

---
+++ MakeupRoutine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeupRoutine
+//
+//  Created by Keylin Sanchez on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakeupRoutine/MakeupRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeupRoutineApp.swift
+//  MakeupRoutine
+//
+//  Created by Keylin Sanchez on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MakeupRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
